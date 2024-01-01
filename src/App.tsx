3520c0b0bc33a7e1eecf9b2a8d0a2bbcd09af98b
JoaoAlisonTI <joaoalison.ti@gmail.com>
2024-01-01 18:12:55 -0300

    fisrt commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <div className="w-full h-screen m-0 p-0 box-border bg-slate-950 text-white">
+        <h1>Olá mundo</h1>
+      </div>
+    )
+}
+
+export default App
